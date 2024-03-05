09830d39f403cd0b502341c17a073e77ef75d8be
Chris <114956449+bc-csewell@users.noreply.github.com>
2024-03-05 11:41:39 +0000

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,35 @@
+import json
+import urllib3
+
+def lambda_handler(event, context):
+    
+    http = urllib3.PoolManager()
+
+    response = http.request('GET',
+                        'https://s3.eu-west-2.amazonaws.com/build-circle/characters.json',
+                        retries = False)
+    
+    characters = json.loads(response.data.decode("utf-8"))
+
+    if "queryStringParameters" in event: 
+        queries = event["queryStringParameters"]
+        
+        hero_name = queries['hero']
+        villain_name = queries['villain']
+        
+        hero = None
+        villain = None
+        winner = None
+        
+        for character in characters["items"]:
+            if character["name"] == hero_name:
+                hero = character
+            if character["name"] == villain_name:
+                villain = character
+                
+        winner = hero if hero["score"] >= villain["score"] else villain
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps(winner)
+    }
