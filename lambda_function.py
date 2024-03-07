9eb456eec6aef263a386e125c207d89a074f529d
Marco De Bortoli <137501693+bc-debo@users.noreply.github.com>
2024-03-07 11:55:56 +0000

    Trim trailing spaces

--- lambda_function.py
+++ lambda_function.py
@@ -3,33 +3,33 @@ import urllib3
 import os
 
 def lambda_handler(event, context):
-    
+
     http = urllib3.PoolManager()
 
     response = http.request('GET',
                         os.environ["data_url"],
                         retries = False)
-    
+
     characters = json.loads(response.data.decode("utf-8"))
 
-    if "queryStringParameters" in event: 
+    if "queryStringParameters" in event:
         queries = event["queryStringParameters"]
-        
+
         hero_name = queries['hero']
         villain_name = queries['villain']
-        
+
         hero = None
         villain = None
         winner = None
-        
+
         for character in characters["items"]:
             if character["name"] == hero_name:
                 hero = character
             if character["name"] == villain_name:
                 villain = character
-                
+
         winner = hero if hero["score"] >= villain["score"] else villain
-        
+
     return {
         'statusCode': 200,
         'body': json.dumps(winner)
